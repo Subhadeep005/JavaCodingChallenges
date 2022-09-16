eccb532aa5ce18cf827c4c21103663a4d572eb84
Subhadeep005 <subhadeepmallick781@gmail.com>
2022-09-16 18:25:40 +0530

    Added New Practice

--- JavaPractice/src/my/practice/java/FindSum.java
+++ JavaPractice/src/my/practice/java/FindSum.java
@@ -1,17 +1,32 @@
-package practice.test.java;
+package my.practice.java;
 
 public class FindSum {
 
 	public static void main(String[] args) {
 		
-		int arr[]= {2,7,11,15};
+		int arr[]= {2,11,15,7};
+		int arr1[]= {2,7,11,15};
 		
-		int targetValue =9;
+		int targetValue =26;
+		
+		i=0 a(0)
+		j=15 
+		9-2=7
+		
+		7,0
+		2,1
+		6,2
+		2,3
 		
 		for(int i=0; i<arr.length;i++) {
+			int temp =arr[i];
 			
-			if(arr[i] +arr[i+1]==targetValue)
+			if(targetValue-arr[i] ==  arr[])
+				
 				System.out.println("["+arr[i]+","+arr[i+1]+"]");
+			
+			
+			
 			/*
 			 * for(int j=i+1; j<arr.length;j++) { if(arr[i]+arr[j] == targetValue) {
 			 * System.out.println("["+arr[i]+","+arr[j]+"]"); } }
